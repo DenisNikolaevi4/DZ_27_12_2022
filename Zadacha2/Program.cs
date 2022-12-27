e7dae5ad61193eef7848d36892d6695d885935b4
SkyLearn <ba_de@mail.ru>
2022-12-27 17:57:21 +0300

    готово

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num_cub;
+int index=1;
+while (index<=number)
+{
+    num_cub=index*index*index;
+    index++;
+    Console.WriteLine($"Куб числа {index-1} -> {num_cub}");
+
+}
+
